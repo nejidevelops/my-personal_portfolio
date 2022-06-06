0e9b86e522fa248bc308bb5887e7321b3d78c0b0
NewtonOmbese <newtonombese1@gmail.com>
2022-06-06 09:23:05 +0300

    explore js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 const conect = document.getElementById("connect")
+const explor = document.getElementById("explore")
 
 conect.addEventListener("click", function con(){
     alert("Hey you've connected with Newton")
+})
+
+explor.addEventListener("click", function exp(){
+    alert("Take your time to explore my portfolio")
 })
